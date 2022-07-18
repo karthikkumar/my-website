67881d828f81aa46c1492ad0f830aec81118c8bc
Karthik Kumar <karthik.krishnamoorthy@nagarro.com>
2022-07-18 23:20:34 +0530

    Highlight curremt path's nav item

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,32 +1,64 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function NavBar() {
+function NavItem({ name }) {
+  const router = useRouter();
+  const path = `/${name}`;
+  const className = "nav-item" + (router.pathname === path ? " highlight" : "");
   return (
     <>
-      <div id="navBar">
+      <Link href={path}>
+        <a className={className}>{name}</a>
+      </Link>
+      <style jsx>
+        {`
+          .nav-item {
+            border-bottom: 2px solid transparent;
+            text-decoration: none;
+            color: #f5f5f5;
+          }
+
+          .highlight {
+            border-bottom: 2px solid #007ced;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <>
+      <div id="nav-bar">
         <div id="home">
           <Link href="/">
-            <Image src="/images/home.png" width={45} height={45} alt="home" />
+            <Image
+              src={
+                router.pathname === "/"
+                  ? "/images/home-highlighted.png"
+                  : "/images/home.png"
+              }
+              width={45}
+              height={45}
+              alt="home"
+            />
           </Link>
+          <span />
         </div>
-        <div id="navItems">
-          <Link href="/profile">
-            <a className="navItem">profile</a>
-          </Link>
-          <Link href="/work">
-            <a className="navItem">work</a>
-          </Link>
-          <Link href="/blog">
-            <a className="navItem">blog</a>
-          </Link>
+        <div id="nav-items">
+          <NavItem name="profile" />
+          <NavItem name="work" />
+          <NavItem name="blog" />
         </div>
         <div id="lamp">
           <Image src="/images/lamp.png" width={35} height={68} alt="lamp" />
         </div>
       </div>
       <style jsx>{`
-        #navBar {
+        #nav-bar {
           height: 80px;
           width: 90%;
           border-bottom: 2px solid #2c2c2c;
@@ -46,19 +78,16 @@ export default function NavBar() {
           margin-top: 1rem;
         }
 
-        #navItems {
+        #nav-items {
+          position: relative;
+          top: 2px;
           display: flex;
           gap: 3rem;
         }
 
-        .navItem {
-          text-decoration: none;
-          color: #f5f5f5;
-        }
-
         #home {
           position: relative;
-          top: 8px;
+          top: 7px;
           cursor: pointer;
         }
 
