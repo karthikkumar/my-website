01420f4a43eafb2428e6dec12f543bc6efc05764
Karthik Kumar <karthik.krishnamoorthy@nagarro.com>
2022-09-28 10:18:14 +0530

    Remove determining dark mode preference

--- common/hooks.js
+++ common/hooks.js
@@ -1,16 +1,7 @@
 import { useCallback, useState } from "react";
 
-const getInitialValue = (usersInitialState) => {
-  const preferDarkQuery = "(prefers-color-scheme: dark)";
-  const mql = globalThis.matchMedia
-    ? globalThis.matchMedia(preferDarkQuery)
-    : {};
-  const isColorSchemeQuerySupported = mql.media === preferDarkQuery;
-  return isColorSchemeQuerySupported ? mql.matches : usersInitialState;
-};
-
 function useDarkMode(initialValue) {
-  const [state, setState] = useState(getInitialValue(initialValue));
+  const [state, setState] = useState(initialValue);
   return [
     state,
     useCallback(() => setState((current) => !current), [setState]),
